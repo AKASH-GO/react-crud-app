f13bc731dd7ec85ebbdafac54b6cd0ac1103106f
AKASH <88474098+AKASH-GO@users.noreply.github.com>
2023-09-27 15:12:00 +0530

    second

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,27 @@
-
+import React from "react";
+import {BrowserRouter, Route, Routes} from "react-router-dom";
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+import './App.css';
+import Home from "./pages/Home";
+import AddEdit from "./pages/AddEdit";
+import View from "./pages/View";
+import About from "./pages/About";
 
 function App() {
   return (
+    <BrowserRouter>
     <div className="App">
-      <h2>The CRUD Application</h2>
+      <ToastContainer />
+      <Routes>
+        <Route exact path="/" element={<Home></Home>} />
+            <Route path="/add" element={<AddEdit></AddEdit>} />
+            <Route path="/update/:id" element={<AddEdit></AddEdit>} />
+            <Route path="/view/:id" element={<View></View>} />
+            <Route path="/about" element={<About></About>} /> 
+        </Routes>
     </div>
+     </BrowserRouter>
   );
 }
 
